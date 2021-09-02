da5b8af5f3966c7ea38cfcfea91729158595dd76
Marciely Santos <marciely.tech@gmail.com>
2021-09-02 12:57:54 -0300

    responsividade

---
+++ js/combo-navi.js
@@ -0,0 +1,39 @@
+// Jquery with no conflict
+jQuery(document).ready(function($) {
+	
+   	//##########################################
+	// Create Combo Navi
+	//##########################################	
+		
+	// Create the dropdown base
+	$("<select id='comboNav' />").appendTo("#combo-holder");
+	
+	// Create default option "Go to..."
+	$("<option />", {
+		"selected": "selected",
+		"value"   : "",
+		"text"    : "Navegação"
+	}).appendTo("#combo-holder select");
+	
+	// Populate dropdown with menu items
+	$("#nav a").each(function() {
+		var el = $(this);		
+		var label = $(this).parent().parent().attr('id');
+		var sub = (label == 'nav') ? '' : '- ';
+		
+		$("<option />", {
+		 "value"   : el.attr("href"),
+		 "text"    :  sub + el.text()
+		}).appendTo("#combo-holder select");
+	});
+
+	
+	//##########################################
+	// Combo Navigation action
+	//##########################################
+	
+	$("#comboNav").change(function() {
+	  location = this.options[this.selectedIndex].value;
+	});
+  
+});
